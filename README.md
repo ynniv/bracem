f72e89cf2148cc83176f6f063201c64780a3d3ca
Vinny Fiano <ynniv@ynniv.com>
2013-07-09 11:09:33 -0400

    fixed escaped quotes in quoted string. added test. made example fancier

--- README.md
+++ README.md
@@ -15,11 +15,11 @@ Distributed under the Eclipse Public License, the same as Clojure.
 ## Example
 
     { h1 bracem }
-    { p bracem is a markup language based on braces and keywords 
-        that looks a lot like lisp. The important rules are that 
-        \{ braces \} define forms, each form starts with a word, 
-        :keyword "value" pairs follow, and the body can contain 
-        a mixture of text and forms.}
+    { p :class intro :onclick "if (1) { alert('hello there!');}" bracem is a 
+        markup language based on braces and keywords that looks a lot like 
+        lisp. The important rules are that \{ braces \} denote forms, each 
+        form starts with a word, :keyword "value" pairs follow, and the 
+        remainder of the form can contain a mixture of text and forms. }
 
 ## Syntax
 
--- test/bracem/test/core.clj
+++ test/bracem/test/core.clj
@@ -2,5 +2,7 @@
   (:use [bracem.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest basic-smoke
+  (is true (= "<h1>bracem </h1><p onclick=\"if (1) { alert('hello there!');}\" class=\"intro\">bracem is a markup language based on braces and keywords \n        that looks a lot like lisp. The important rules are that \n        { braces } define forms, each form starts with a word, \n        :keyword \"value\" pairs follow, and the body can contain \n        a mixture of text and forms. </p>"
+              (process-stream "    { h1 bracem }\n    { p :class intro :onclick \"if (1) { alert('hello there!');}\"\n        bracem is a markup language based on braces and keywords \n        that looks a lot like lisp. The important rules are that \n        \\{ braces \\} define forms, each form starts with a word, \n        :keyword \"value\" pairs follow, and the body can contain \n        a mixture of text and forms. }\n" render-sgml identity))))
+
